e41c7267736edb7d3d913ee6486931c30c19129b
Robert Meis <robert.meis@colorado.edu>
2017-10-25 00:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function happyHolidayTo(holiday, name) {
 return "Happy " + holiday + ", " + name + "!";
 
 }
+
+
+function holidayCountdown(holiday, days) {
+
+
+  return "It's " + days + " days until " + holiday + "!";
+
+  
+}
